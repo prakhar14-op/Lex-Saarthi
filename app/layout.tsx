3aef8e0e00601cd0e653e6e5413de0fa5f5d3ea6
Prakhar Sharma <prakharsharma_mc24b06_001@dtu.ac.in>
2026-06-02 14:32:16 +0530

    added something in layout.tsx like keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,30 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import StructuredData from "@/components/StructuredData";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 export const metadata: Metadata = {
   title: "Lex Saarthi",
-  description: "Your legal AI companion",
+  description: "Lex Saarthi is an AI-powered legal assistant that helps students, citizens, and professionals with legal research, legal awareness, document guidance, and law-related information.",
   verification: {
     google: "FLvs_8zKJDfqNLiNsQtKjte7c-2wWW_YwKxU_97rqGs",
+  },
+  keywords: [
+    "Lex Saarthi",
+    "AI Legal Assistant",
+    "Legal Research",
+    "Indian Law",
+    "Legal Chatbot",
+    "Law Student Assistant"
+  ],
+  openGraph: {
+    title: "Lex Saarthi",
+    description:
+      "AI-powered legal assistant for legal research and guidance.",
+    url: "https://lex-saarthi.vercel.app",
+    siteName: "Lex Saarthi",
+    type: "website",
   },
 };
 
@@ -20,6 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} font-sans antialiased bg-transparent`}>
+        <StructuredData />
         <video
           autoPlay
           loop
